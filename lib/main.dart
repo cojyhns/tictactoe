71739a98f481536216075c461b4c0eec0d583340
cojyhns <cojyboi@gmail.com>
2025-11-24 22:19:09 +0200

    part 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
 
@@ -19,27 +24,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String displayExOh = '';
+  // 9 squares
+  List<String> board = ['', '', '', '', '', '', '', '', ''];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: GridView.builder(
         itemCount: 9,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
         ),
-        itemBuilder: (BuildContext context, int index) {
+        itemBuilder: (context, index) {
           return GestureDetector(
-            onTap: _tapped,
+            onTap: () => _tapped(index),
             child: Container(
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey[700]!, width: 1),
               ),
               child: Center(
                 child: Text(
-                  displayExOh,
-                  style: TextStyle(color: Colors.white, fontSize: 40),
+                  board[index],
+                  style: const TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
@@ -49,9 +61,11 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _tapped() {
+  void _tapped(int index) {
     setState(() {
-      displayExOh = 'o';
+      if (board[index] == '') {
+        board[index] = 'o';
+      }
     });
   }
 }
